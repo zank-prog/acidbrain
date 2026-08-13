2fb941bc1581045d99e8b8d7edddb805590b0211
zank-prog <ezankiewicz@gmail.com>
2026-08-13 14:46:45 -0700

    initialize tracker

--- software/main/ui.go
+++ software/main/ui.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func runUI() {
-	myApp := app.New()
+	myApp := app.NewWithID("com.acidbrain.app")
 	window := myApp.NewWindow("ACIDBRAIN")
 
-	content := container.NewVBox(
-		widget.NewLabel("ACIDBRAIN TRACKER"),
-	)
+	trackerText := buildTrackerText()
+	label := widget.NewLabel(trackerText)
+	label.TextStyle = fyne.TextStyle{Monospace: true}
+
+	content := container.NewVBox(label)
 
 	window.SetContent(content)
+	window.Resize(fyne.NewSize(400, 600))
 	window.ShowAndRun()
+}
+
+func buildTrackerText() string {
+	text := "STEP	NOTE\n"
+	for i := 0; i < 32; i++ {
+		text += fmt.Sprintf("%2d	---\n", i+1)
+	}
+	return text
 }
