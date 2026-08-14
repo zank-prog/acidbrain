aa29bd6d8b00061def1aedbd759d25ca0f1b7fec
zank-prog <ezankiewicz@gmail.com>
2026-08-14 07:59:44 -0700

    tracker, variable seeding

--- software/main/ui.go
+++ software/main/ui.go
@@ -13,7 +13,19 @@ func runUI() {
 	myApp := app.NewWithID("com.acidbrain.app")
 	window := myApp.NewWindow("ACIDBRAIN")
 
-	trackerText := buildTrackerText()
+	var samplePattern Pattern
+	samplePattern.reseed(12345)
+	sampleParams := Params{
+		Key:        2,
+		ScaleIdx:   1,
+		Steps:      32,
+		Density:    0.75,
+		Complexity: 0.5,
+		Gate:       0.5,
+		Bpm:        120,
+	}
+
+	trackerText := buildTrackerText(samplePattern, sampleParams)
 	label := widget.NewLabel(trackerText)
 	label.TextStyle = fyne.TextStyle{Monospace: true}
 
@@ -22,12 +34,32 @@ func runUI() {
 	window.SetContent(content)
 	window.Resize(fyne.NewSize(400, 600))
 	window.ShowAndRun()
+
 }
 
-func buildTrackerText() string {
-	text := "STEP	NOTE\n"
-	for i := 0; i < 32; i++ {
-		text += fmt.Sprintf("%2d	---\n", i+1)
+func noteName(midiNote int) string {
+	names := []string{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"}
+	octave := midiNote/12 - 1
+	name := names[midiNote%12]
+	return fmt.Sprintf("%s%d", name, octave)
+}
+
+func buildTrackerText(pattern Pattern, params Params) string {
+	text := "STEP  NOTE\n"
+	for i := 0; i < params.Steps; i++ {
+		step := pattern.realize(i, params)
+		if step.Active {
+			marker := ""
+			if step.Accent {
+				marker += " *"
+			}
+			if step.Slide {
+				marker += " ~"
+			}
+			text += fmt.Sprintf("%2d    %-4s%s\n", i+1, noteName(step.Note), marker)
+		} else {
+			text += fmt.Sprintf("%2d    ---\n", i+1)
+		}
 	}
 	return text
 }
